2877635fd2f85d1b2dd414c3df4b896a8490fc8b
Akhil <akhilselukar.work@gmail.com>
2022-01-14 23:30:58 +0530

    second commit

---
+++ src/main/java/com/acecorp/dicdtest/DicdtestApplication.java
@@ -0,0 +1,13 @@
+package com.acecorp.dicdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DicdtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DicdtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/acecorp/dicdtest/DicdtestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acecorp.dicdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DicdtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
